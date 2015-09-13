0790c806ffa66c6d3f40b69d522f924533a98bf0
boojongmin <boojongmin@gmail.com>
2015-09-13 22:48:07 +0900

    functional test will make work item

--- book/TestDrivenDevelopmentwithPython/example/functional_test.py
+++ book/TestDrivenDevelopmentwithPython/example/functional_test.py
@@ -14,6 +14,19 @@ class NewVisitorTest(unittest.TestCase):
     self.browser.get('http://localhost:8000')
 
     self.assertIn('To-Do', self.browser.title)
+    header_text = self.browser.find_element_by_tag_name('h1').header_text
+    self.assertEqual(
+      inputbox.get_attribute('placeholder'),
+      'insert work item'
+    )
+    inputbox.send_keys(Keys.ENTER)
+
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_element_by_tag_name('tr')
+    self.assertTrue(
+      any(row.text == '1: buy bird' for row in rows)
+
+    )
 
     self.fail('Finish the test!')
 
