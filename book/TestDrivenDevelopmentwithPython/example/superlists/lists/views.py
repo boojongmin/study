f8fe7582a3d9e7d114260409e482cd7d5aeb290c
boojongmin <boojongmin@gmail.com>
2015-09-13 21:46:57 +0900

    update

---
+++ book/TestDrivenDevelopmentwithPython/example/superlists/lists/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# home_page = None
+def home_page(request):
+    return HttpResponse(b'<html><title>To-Do lists</title></html>');
